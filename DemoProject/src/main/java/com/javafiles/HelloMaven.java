d34032e09fb4222d4e1f6ff4040a60acd3576f55
bhushan8590 <bhushan8590@outlook.com>
2018-05-22 20:01:47 -0400

    New welcome message added

--- DemoProject/src/main/java/com/javafiles/HelloMaven.java
+++ DemoProject/src/main/java/com/javafiles/HelloMaven.java
@@ -5,6 +5,7 @@ public class HelloMaven {
 	public static void main(String[] args) {
 		System.out.println("Hello Maven...!!");
 		System.out.println("Hello STS...!!");
+		System.out.println("Hello All Welcome...!!");
 	}
 
 }
