e14f3ddc74aae42bd937d1157f25522c89ee5bc6
bhushan8590 <bhushan8590@outlook.com>
2020-05-22 15:39:32 -0400

    move sout to top

--- DemoProject/src/main/java/com/javafiles/NewClass.java
+++ DemoProject/src/main/java/com/javafiles/NewClass.java
@@ -5,9 +5,9 @@ public class NewClass {
 	int b;
 	
 	private void newMethod() {
+		System.out.println("New Message from Sub-Branch");
 		System.out.println("Test code");
 		System.out.println("New Message from Master");
-		System.out.println("New Message from Sub-Branch");
 		System.out.println("New Revert Demo");
 
 	}
