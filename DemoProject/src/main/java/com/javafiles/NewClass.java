a91b5f703ab2ae81017daf594c4297b631d492e0
bhushan8590 <bhushan8590@outlook.com>
2020-08-26 13:21:37 -0400

    Testing merge conflict

--- DemoProject/src/main/java/com/javafiles/NewClass.java
+++ DemoProject/src/main/java/com/javafiles/NewClass.java
@@ -9,6 +9,7 @@ public class NewClass {
 		System.out.println("Test code");
 		System.out.println("New Message from Master");
 		System.out.println("New Revert Demo");
+		System.out.println("Test merge conflict");
 
 	}
 }
