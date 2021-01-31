31e155de36e26b07abc804437cd7152482bd85d8
bhushan8590 <bhushan8590@outlook.com>
2021-01-31 13:41:26 -0500

    Adding FTR-308

--- DemoProject/src/main/java/com/javafiles/FeatureSevenEightNine.java
+++ DemoProject/src/main/java/com/javafiles/FeatureSevenEightNine.java
@@ -5,8 +5,10 @@ public class FeatureSevenEightNine {
   public void fetureFunction() {
 	  System.out.println("test");
 	  System.out.println("test-FTR");
+	  System.out.println("test-FTR");
   }
   public void newFun() {
+	  System.out.println("Stay Happy..!!");
 	  System.out.println("Stay Happy..!!");
   }
   
--- DemoProject/src/main/java/com/javafiles/HelloMaven.java
+++ DemoProject/src/main/java/com/javafiles/HelloMaven.java
@@ -7,6 +7,7 @@ public class HelloMaven {
 		System.out.println("Hello STS...!!");
 		System.out.println("Hello All Welcome...!!");
 		System.out.println("Another Hello...!!");
+		System.out.println("Hello FTR-308...!!");
 		welcome();
 	}
 
